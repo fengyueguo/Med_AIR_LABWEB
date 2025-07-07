535ace7dc241010910ac7e9be876854cb4cf032e
fengyueguo <150006398+fengyueguo@users.noreply.github.com>
2025-07-07 05:29:58 +0000

    Deploying to gh-pages from @ fengyueguo/Med_AIR_LABWEB@af1a167cb8f144c924389ec76696a44593338f43 🚀

--- _scripts/publication.js
+++ _scripts/publication.js
@@ -1,26 +1,65 @@
 
 {
+    // function filterElements(tag) {
+    //     var elements = document.getElementsByClassName("publication");
+    //     var years = document.getElementsByClassName("paper-year")
+    //     for (var i = 0; i < elements.length; i++) {
+    //         var element = elements[i];
+    //         if (tag === "all" || element.classList.contains(tag)) {
+    //             element.style.display = "flex";
+    //         } else {
+    //             element.style.display = "none";
+    //         }
+    //     }
+    //     for (var i = 0; i < years.length; i++) {
+    //         var year = years[i];
+    //         if (tag === "all") {
+    //             year.style.display = "";
+    //             year.style.position = "relative";
+    //             year.style.left = "-45%";
+    //         }
+    //         else {
+    //             year.style.display = "none";
+    //         }
+    //     }
+    // }
+
     function filterElements(tag) {
-        var elements = document.getElementsByClassName("publication");
-        var years = document.getElementsByClassName("paper-year")
-        for (var i = 0; i < elements.length; i++) {
-            var element = elements[i];
-            if (tag === "all" || element.classList.contains(tag)) {
-                element.style.display = "flex";
-            } else {
-                element.style.display = "none";
-            }
-        }
-        for (var i = 0; i < years.length; i++) {
-            var year = years[i];
-            if (tag === "all") {
-                year.style.display = "";
-                year.style.position = "relative";
-                year.style.left = "-45%";
-            }
-            else {
-                year.style.display = "none";
-            }
+    var publications = document.getElementsByClassName("publication");
+    var years = document.getElementsByClassName("paper-year");
+    var yearContainers = document.querySelectorAll("[class^='paper-']"); // e.g., paper-2025
+
+    // 控制 publication 显示
+    for (var i = 0; i < publications.length; i++) {
+        var pub = publications[i];
+        if (tag === "all" || pub.classList.contains(tag)) {
+            pub.style.display = "flex";
+        } else {
+            pub.style.display = "none";
         }
     }
+
+    // 显示年份标题
+    for (var i = 0; i < years.length; i++) {
+        var year = years[i];
+        if (tag === "all") {
+            year.style.display = "block";
+            year.style.position = "relative";
+            year.style.left = "0";  // 修复你之前设置的 -45%
+        } else {
+            year.style.display = "none";
+        }
+    }
+
+    // 显示每个年份容器（如 paper-2025）
+    for (var i = 0; i < yearContainers.length; i++) {
+        var container = yearContainers[i];
+        if (tag === "all") {
+            container.style.display = "block";
+        } else {
+            container.style.display = ""; // 可设置为 none 也行，看你结构
+        }
+    }
+}
+
 }
