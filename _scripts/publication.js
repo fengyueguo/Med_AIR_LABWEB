05679498050e87721eca194b58e5a4cb01f975a5
fengyueguo <150006398+fengyueguo@users.noreply.github.com>
2025-07-07 09:18:22 +0000

    Deploying to gh-pages from @ fengyueguo/Med_AIR_LABWEB@31b90d9819386353e42c39e3fad4677e61bbe866 🚀

--- _scripts/publication.js
+++ _scripts/publication.js
@@ -14,8 +14,7 @@
     //     for (var i = 0; i < years.length; i++) {
     //         var year = years[i];
     //         if (tag === "all") {
-    //             // year.style.display = "";
-    //             year.style.display = "block";
+    //             year.style.display = "";
     //             year.style.position = "relative";
     //             year.style.left = "-45%";
     //         }
@@ -25,29 +24,25 @@
     //     }
     // }
 
-function filterElements(tag) {
-    var elements = document.getElementsByClassName("publication");
-    var years = document.getElementsByClassName("paper-year");
-
-    // 控制 publication 显示
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        if (tag === "all" || element.classList.contains(tag)) {
-            element.style.display = "flex";
-        } else {
-            element.style.display = "none";
+    function filterElements(tag) {
+        var elements = document.getElementsByClassName("publication");
+        var years = document.getElementsByClassName("paper-year");
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            if (tag === "all" || element.classList.contains(tag)) {
+                element.style.display = "flex";
+            } else {
+                element.style.display = "none";
+            }
+        }
+    
+        for (var i = 0; i < years.length; i++) {
+            var year = years[i];
+            if (tag === "all") {
+                year.style.display = "block";
+            } else {
+                year.style.display = "none";
+            }
         }
     }
-
-    // 控制 h5.year 的显示
-    for (var i = 0; i < years.length; i++) {
-        var year = years[i];
-        if (tag === "all") {
-            year.style.display = "block";
-        } else {
-            year.style.display = "none";
-        }
-    }
-}
-
 }
