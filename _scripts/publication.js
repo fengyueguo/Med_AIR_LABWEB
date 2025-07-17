d98c1cbc7a198e568812f52093302b6a7f8ba1a8
fengyueguo <150006398+fengyueguo@users.noreply.github.com>
2025-07-17 13:40:26 +0800

    Update publication.js

--- _scripts/publication.js
+++ _scripts/publication.js
@@ -24,24 +24,25 @@
     //     }
     // }
 
-   function filterElements(tag) {
-    var elements = document.getElementsByClassName("publication");
-    var years = document.getElementsByClassName("paper-year");
-
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        if (tag === "all" || element.classList.contains(tag)) {
-            element.classList.add("show");
-        } else {
-            element.classList.remove("show");
+     function filterElements(tag) {
+            var elements = document.getElementsByClassName("publication");
+            var years = document.getElementsByClassName("paper-year");
+            for (var i = 0; i < elements.length; i++) {
+                var element = elements[i];
+                if (tag === "all" || element.classList.contains(tag)) {
+                    element.style.display = "flex";
+                } else {
+                    element.style.display = "none";
+                }
+            }
+        
+            for (var i = 0; i < years.length; i++) {
+                var year = years[i];
+                if (tag === "all") {
+                    year.style.display = "block";
+                } else {
+                    year.style.display = "none";
+                }
+            }
         }
-    }
-
-    for (var i = 0; i < years.length; i++) {
-        var year = years[i];
-        year.style.display = (tag === "all") ? "block" : "none";
-    }
-}
-
-
 }
