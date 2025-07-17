dffe025018df2c063edba8a9d8550ee1857bc60d
fengyueguo <150006398+fengyueguo@users.noreply.github.com>
2025-07-17 19:48:02 +0000

    Deploying to gh-pages from @ fengyueguo/Med_AIR_LABWEB@25208171fb379fe35270f4434e4f397cb07c61fc 🚀

--- _scripts/publication.js
+++ _scripts/publication.js
@@ -24,49 +24,27 @@
     //     }
     // }
 
-     // function filterElements(tag) {
-     //        var elements = document.getElementsByClassName("publication");
-     //        var years = document.getElementsByClassName("paper-year");
-     //        for (var i = 0; i < elements.length; i++) {
-     //            var element = elements[i];
-     //            if (tag === "all" || element.classList.contains(tag)) {
-     //                element.style.display = "flex";
-     //            } else {
-     //                element.style.display = "none";
-     //            }
-     //        }
+     function filterElements(tag) {
+            var elements = document.getElementsByClassName("publication");
+            var years = document.getElementsByClassName("paper-year");
+            for (var i = 0; i < elements.length; i++) {
+                var element = elements[i];
+                if (tag === "all" || element.classList.contains(tag)) {
+                    element.style.display = "flex";
+                } else {
+                    element.style.display = "none";
+                }
+            }
         
-     //        for (var i = 0; i < years.length; i++) {
-     //            var year = years[i];
-     //            if (tag === "all") {
-     //                year.style.display = "block";
-     //            } else {
-     //                year.style.display = "none";
-     //            }
-     //        }
-     //    }
-
-function filterElements(tag) {
-    const elements = document.getElementsByClassName("publication");
-    const years = document.getElementsByClassName("paper-year");
-
-    for (let i = 0; i < elements.length; i++) {
-        const el = elements[i];
-        if (tag === "all" || el.classList.contains(tag)) {
-            el.classList.remove("filtered-out");
-        } else {
-            el.classList.add("filtered-out");
+            for (var i = 0; i < years.length; i++) {
+                var year = years[i];
+                if (tag === "all") {
+                    year.style.display = "block";
+                } else {
+                    year.style.display = "none";
+                }
+            }
         }
-    }
-
-    for (let i = 0; i < years.length; i++) {
-        if (tag === "all") {
-            years[i].classList.remove("filtered-out");
-        } else {
-            years[i].classList.add("filtered-out");
-        }
-    }
-}
 
 
 }
